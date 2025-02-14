db39145cdd607dbd7050c1d754ed9f2eea1a6b1d
Rituraj67 <rituraj672000@gmail.com>
2025-02-14 17:07:48 +0530

    ok

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ import verifyFirebaseToken from "./middlewares/authMiddleware.js";
 import authRouter from "./routes/authRoutes.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import { upload } from "./config/multer.js";
-import { uploadToCloudinary } from "./config/cloudinary.js";
 
 dotenv.config();
 app.use(express.json());
@@ -26,23 +24,11 @@ app.use(
   })
 );
 
-app.get("/", (req, res) =>
+app.get("/api", (req, res) =>
   res.json({ message: "Hello from Lambda-Express @Genoviq!" })
 );
 
-app.post("/uploadimage", upload.single("image"), async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
 
-    const result = await uploadToCloudinary(req.file.buffer);
-    res.json({ message: "Upload successful", ...result });
-  } catch (error) {
-    console.error("Cloudinary Upload Error:", error);
-    res.status(500).json({ error: "Upload failed" });
-  }
-});
 
 app.use("/auth", authRouter);
 app.use("/companies", verifyFirebaseToken, companyRoutes);
