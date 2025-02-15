01f494151b1d4bc7494020b165ffedc5e7cb924e
Rituraj67 <rituraj672000@gmail.com>
2025-02-15 17:25:44 +0530

    ts, tailwind confit correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
-import "./app.css"
+import "./App.css"
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import Login from "./pages/login";
